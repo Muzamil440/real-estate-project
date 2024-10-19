af6ba80450ca63aaeb6eb5d31f4b34cac1c9c46e
Muzamil Hussain <muzamil440h@gmail.com>
2024-10-19 18:33:00 +0500

    create pages and routes

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   { ignores: ['dist'] },
@@ -15,6 +15,7 @@ export default [
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
+        allowImportExportEverywhere: true,
       },
     },
     settings: { react: { version: '18.3' } },
@@ -35,4 +36,4 @@ export default [
       ],
     },
   },
-]
+];
