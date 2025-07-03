5d1eae2d874465dc4174d013e3606d849b2d46cc
Muzamil Hussain <muzamil440h@gmail.com>
2025-07-03 12:11:58 +0500

    Switching from  'Sahand' to 'Muzamil'

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
   const [rentListings, setRentListings] = useState([]);
   const [saleListings, setSaleListings] = useState([]);
   SwiperCore.use([Navigation]);
-  console.log(offerListings);
+  // console.log(offerListings);
 
   useEffect(() => {
     const fetchOfferListings = async () => {
@@ -55,7 +55,7 @@ export default function Home() {
           place with ease
         </h1>
         <div className="text-gray-400 text-xs sm:text-sm">
-          Sahand Estate is the best place to find your next perfect place to
+          Muzamil Estate is the best place to find your next perfect place to
           live.
           <br />
           We have a wide range of properties for you to choose from.
